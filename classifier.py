bdeebfac0f12e1607410895f3870ad8da83ffed0
Matt Yuan <matthewfelixyuan@gmail.com>
2019-09-02 17:58:03 -0400

    add final evaluation

--- classifier.py
+++ classifier.py
@@ -10,10 +10,15 @@ y = numpy.load("classes.npy")
 # Split the data into a training set and a test set.
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-# Train a classifier for each given regularization strength and record its accuracy.
-results = []
+# Train a classifier and print a 95% confidence interval of score for each given regularization strength.
+print("\nCross-Validation Scores:")
 for i in range(1, len(sys.argv)):
     classifier = LogisticRegression(penalty="l2", C=float(sys.argv[i]), solver="lbfgs", multi_class="ovr")
     scores = cross_val_score(classifier, X_train, y_train, cv=5)
-    results.append("%0.3f (+/- %0.3f)" % (scores.mean(), scores.std() * 2))
-print(results)
+    print(str(i) + " %0.3f (+/- %0.3f)" % (scores.mean(), scores.std() * 2))
+
+# Calculate final performance of user-chosen regularization strength.
+choice = int(input("enter a row number: "))
+classifier = LogisticRegression(penalty="l2", C=float(sys.argv[choice]), solver="lbfgs", multi_class="ovr")
+classifier.fit(X_train, y_train)
+print("\nFinal Score: %0.3f" % (classifier.score(X_test, y_test)))
